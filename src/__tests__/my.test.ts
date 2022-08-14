f1388633f71260ffaaf33d2b8e489f7ee9bd22f1
Andres Correa Casablanca <castarco@coderspirit.xyz>
2022-08-14 20:48:45 +0200

    test: configure dummy tests

---
+++ src/__tests__/my.test.ts
@@ -0,0 +1,12 @@
+import assert from "node:assert";
+import test from "node:test";
+
+test("top level test", async (t) => {
+	await t.test("subtest 1", (t) => {
+		assert.strictEqual(1, 1);
+	});
+
+	await t.test("subtest 2", (t) => {
+		assert.strictEqual(2, 2);
+	});
+});
