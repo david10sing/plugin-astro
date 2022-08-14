999b1f156dddc410c0bfa6c26019954337127d6e
Andres Correa Casablanca <castarco@coderspirit.xyz>
2022-08-14 20:54:07 +0200

    test: configure tsc as linter

--- src/__tests__/my.test.ts
+++ src/__tests__/my.test.ts
@@ -2,11 +2,11 @@ import assert from "node:assert";
 import test from "node:test";
 
 test("top level test", async (t) => {
-	await t.test("subtest 1", (t) => {
+	await t.test("subtest 1", () => {
 		assert.strictEqual(1, 1);
 	});
 
-	await t.test("subtest 2", (t) => {
+	await t.test("subtest 2", () => {
 		assert.strictEqual(2, 2);
 	});
 });
