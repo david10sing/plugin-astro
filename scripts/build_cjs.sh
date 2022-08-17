e5bd689d1d9edb6d3f3eca26c1fd9020f1b43596
Andres Correa Casablanca <castarco@coderspirit.xyz>
2022-08-17 20:43:21 +0200

    fix: map references inside source files

--- scripts/build_cjs.sh
+++ scripts/build_cjs.sh
@@ -5,5 +5,10 @@ for i in $( ls ./dist/cjs/*.js ); do mv $i ${i%.*}.cjs; done && \
 for i in $( ls ./dist/cjs/*.js.map ); do
   ii="${i%.*}"
   mv $i ${ii%.*}.cjs.map;
+
+	# Fix source file refences inside maps
 	sed -Ei 's/"file":"(.+)\.js"/"file":"\1.cjs"/g' "${ii%.*}.cjs.map";
+
+	# Fix map references inside source files
+	sed -Ei 's/\/\/# sourceMappingURL=(.+)\.js\.map/\/\/# sourceMappingURL=\1.cjs.map/g' "${ii%.*}.cjs";
 done
--- scripts/build_esm.sh
+++ scripts/build_esm.sh
@@ -5,5 +5,10 @@ for i in $( ls ./dist/esm/*.js ); do mv $i ${i%.*}.mjs; done && \
 for i in $( ls ./dist/esm/*.js.map ); do
   ii="${i%.*}"
   mv $i "${ii%.*}.mjs.map";
+
+	# Fix source file refences inside maps
 	sed -Ei 's/"file":"(.+)\.js"/"file":"\1.mjs"/g' "${ii%.*}.mjs.map";
+
+	# Fix map references inside source files
+	sed -Ei 's/\/\/# sourceMappingURL=(.+)\.js\.map/\/\/# sourceMappingURL=\1.mjs.map/g' "${ii%.*}.mjs";
 done
