9f8dde3e0f4570558e48a076bcd5bf880a3b5ed6
Andres Correa Casablanca <castarco@coderspirit.xyz>
2022-08-19 00:08:45 +0200

    chore: enable eslint && prettier

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+const currentDir = __dirname || '.'
+
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2022,
+		tsConfigRootDir: __dirname,
+		project: [
+			`${currentDir}/tsconfig.cjs.json`,
+			`${currentDir}/tsconfig.tests.json`,
+		],
+	},
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:node/recommended-module',
+		'prettier',
+	],
+	rules: {
+		'node/no-missing-import': [
+			'error',
+			{
+				resolvePaths: [
+					`${currentDir}/src`,
+					`${currentDir}/node_modules`,
+					`${currentDir}/node_modules/@types`,
+					`${currentDir}/node_modules/@types/node`,
+				],
+				tryExtensions: ['.ts', '.d.ts'],
+			},
+		],
+		quotes: ['error', 'single', { avoidEscape: true }],
+		'sort-imports': 'error',
+	},
+	ignorePatterns: ['.eslintrc.js'],
+	overrides: [],
+}
--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,57 @@
-import type { AstroConfig, AstroIntegration, RouteData } from "astro";
-import {
-	create as createLyraDB,
-	insert as insertIntoLyraDB,
-	save as saveLyraDB,
-} from "@nearform/lyra";
+// eslint-disable-next-line node/no-missing-import
+import type { AstroConfig, AstroIntegration, RouteData } from 'astro'
 import type {
 	InsertConfig,
 	Lyra,
 	PropertiesSchema,
 	SearchParams,
-} from "@nearform/lyra";
-import { compile } from "html-to-text";
-import { readFileSync, writeFileSync } from "node:fs";
-import { join as joinPath } from "node:path";
+} from '@nearform/lyra'
+import {
+	create as createLyraDB,
+	insert as insertIntoLyraDB,
+	save as saveLyraDB,
+} from '@nearform/lyra'
+import { readFileSync, writeFileSync } from 'node:fs'
+import { compile } from 'html-to-text'
+import { join as joinPath } from 'node:path'
 
 export const defaultSchema: PropertiesSchema = {
-	path: "string",
-	title: "string",
-	h1: "string",
-	content: "string",
-};
+	path: 'string',
+	title: 'string',
+	h1: 'string',
+	content: 'string',
+}
 
-export type PageIndexSchema = typeof defaultSchema;
+export type PageIndexSchema = typeof defaultSchema
 
 export type LyraOptions = Partial<InsertConfig> & {
-	pathMatcher: RegExp;
-	contentSelectors?: string[];
-	searchOptions?: Omit<SearchParams<PageIndexSchema>, "term"> | undefined;
-};
+	pathMatcher: RegExp
+	contentSelectors?: string[]
+	searchOptions?: Omit<SearchParams<PageIndexSchema>, 'term'> | undefined
+}
 
-const PKG_NAME = "@lyrasearch/plugin-astro";
+const PKG_NAME = '@lyrasearch/plugin-astro'
 
 const titleConverter = compile({
-	baseElements: { selectors: ["title"] },
-});
+	baseElements: { selectors: ['title'] },
+})
 const h1Converter = compile({
-	baseElements: { selectors: ["h1"] },
-});
+	baseElements: { selectors: ['h1'] },
+})
 
 const prepareLyraDb = (
 	dbConfig: LyraOptions,
 	pages: { pathname: string }[],
-	routes: RouteData[]
+	routes: RouteData[],
 ): Lyra<PageIndexSchema> => {
 	const contentConverter = compile({
 		baseElements: {
 			selectors:
 				dbConfig.contentSelectors && dbConfig.contentSelectors.length
 					? dbConfig.contentSelectors
-					: ["body"],
+					: ['body'],
 		},
-	});
+	})
 
 	const pathsToBeIndexed = pages
 		.filter(({ pathname }) => dbConfig.pathMatcher.test(pathname))
@@ -60,21 +61,21 @@ const prepareLyraDb = (
 				?.distURL?.pathname,
 		}))
 		.filter(({ generatedFilePath }) => !!generatedFilePath) as {
-		pathname: string;
-		generatedFilePath: string;
-	}[];
+		pathname: string
+		generatedFilePath: string
+	}[]
 
 	const lyraDB = createLyraDB({
 		schema: defaultSchema,
 		...(dbConfig.language ? { defaultLanguage: dbConfig.language } : undefined),
-	});
+	})
 
 	for (const { pathname, generatedFilePath } of pathsToBeIndexed) {
-		const htmlContent = readFileSync(generatedFilePath, { encoding: "utf8" });
+		const htmlContent = readFileSync(generatedFilePath, { encoding: 'utf8' })
 
-		const title = titleConverter(htmlContent) ?? "";
-		const h1 = h1Converter(htmlContent) ?? "";
-		const content = contentConverter(htmlContent);
+		const title = titleConverter(htmlContent) ?? ''
+		const h1 = h1Converter(htmlContent) ?? ''
+		const content = contentConverter(htmlContent)
 
 		insertIntoLyraDB(
 			lyraDB,
@@ -84,36 +85,36 @@ const prepareLyraDb = (
 				h1,
 				content,
 			},
-			dbConfig.language ? { language: dbConfig.language } : undefined
-		);
+			dbConfig.language ? { language: dbConfig.language } : undefined,
+		)
 	}
 
-	return lyraDB;
-};
+	return lyraDB
+}
 
 export const createPlugin = (
-	options: Record<string, LyraOptions>
+	options: Record<string, LyraOptions>,
 ): AstroIntegration => {
-	let config: AstroConfig;
+	let config: AstroConfig
 	return {
 		name: PKG_NAME,
 		hooks: {
-			"astro:config:done": async ({ config: cfg }) => {
-				config = cfg;
+			'astro:config:done': ({ config: cfg }) => {
+				config = cfg
 			},
-			"astro:build:done": async ({ pages, routes }) => {
+			'astro:build:done': ({ pages, routes }) => {
 				for (const [dbName, dbConfig] of Object.entries(options)) {
-					const namedDb = prepareLyraDb(dbConfig, pages, routes);
+					const namedDb = prepareLyraDb(dbConfig, pages, routes)
 
 					writeFileSync(
-						joinPath(config.outDir.pathname, "assets", `lyraDB_${dbName}.json`),
+						joinPath(config.outDir.pathname, 'assets', `lyraDB_${dbName}.json`),
 						JSON.stringify(saveLyraDB(namedDb)),
-						{ encoding: "utf8" }
-					);
+						{ encoding: 'utf8' },
+					)
 				}
 			},
 		},
-	};
-};
+	}
+}
 
-export default createPlugin;
+export default createPlugin
