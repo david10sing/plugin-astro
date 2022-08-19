ddd86694f9e07fef9c1a9b1f003dc24e85b67426
Andres Correa Casablanca <castarco@coderspirit.xyz>
2022-08-19 21:07:42 +0200

    refactor: reorganize build scripts' loops
    Signed-off-by: Andres Correa Casablanca <castarco@coderspirit.xyz>

--- scripts/build_cjs.sh
+++ scripts/build_cjs.sh
@@ -2,15 +2,24 @@
 
 set -e;
 
-tsc -p ./tsconfig.cjs.json && \
-for i in $( ls ./dist/cjs/*.js ); do mv $i ${i%.*}.cjs; done && \
+# Compile
+tsc -p ./tsconfig.cjs.json;
+
+# Process source files
+for i in $( ls ./dist/cjs/*.js ); do
+	# Rename source files
+	mv $i ${i%.*}.cjs;
+
+	# Fix map references inside source files
+	sed -Ei 's/\/\/# sourceMappingURL=(.+)\.js\.map/\/\/# sourceMappingURL=\1.cjs.map/g' "${i%.*}.cjs";
+done;
+
+# Process source maps
 for i in $( ls ./dist/cjs/*.js.map ); do
+	# Rename sourcemap files
 	ii="${i%.*}"
 	mv $i ${ii%.*}.cjs.map;
 
 	# Fix source file refences inside maps
 	sed -Ei 's/"file":"(.+)\.js"/"file":"\1.cjs"/g' "${ii%.*}.cjs.map";
-
-	# Fix map references inside source files
-	sed -Ei 's/\/\/# sourceMappingURL=(.+)\.js\.map/\/\/# sourceMappingURL=\1.cjs.map/g' "${ii%.*}.cjs";
 done
--- scripts/build_esm.sh
+++ scripts/build_esm.sh
@@ -2,19 +2,25 @@
 
 set -e;
 
-tsc -p ./tsconfig.esm.json && \
-for i in $( ls ./dist/esm/*.js ); do mv $i ${i%.*}.mjs; done && \
+tsc -p ./tsconfig.esm.json;
+
+for i in $( ls ./dist/esm/*.js ); do
+	# Rename source files
+	mv $i ${i%.*}.mjs;
+
+	# Fix map references inside source files
+	sed -Ei 's/\/\/# sourceMappingURL=(.+)\.js\.map/\/\/# sourceMappingURL=\1.mjs.map/g' "${i%.*}.mjs";
+
+	# Fix imports in ESM files (we first remove existing extensions)
+	sed -Ei 's/\s+from\s+"\.\/(.+)\.m?js";/ from ".\/\1";/g' "${i%.*}.mjs";
+	sed -Ei 's/\s+from\s+"\.\/(.+)";/ from ".\/\1.mjs";/g' "${i%.*}.mjs";
+done;
+
 for i in $( ls ./dist/esm/*.js.map ); do
+	# Rename sourcemap files
 	ii="${i%.*}"
 	mv $i "${ii%.*}.mjs.map";
 
 	# Fix source file refences inside maps
 	sed -Ei 's/"file":"(.+)\.js"/"file":"\1.mjs"/g' "${ii%.*}.mjs.map";
-
-	# Fix map references inside source files
-	sed -Ei 's/\/\/# sourceMappingURL=(.+)\.js\.map/\/\/# sourceMappingURL=\1.mjs.map/g' "${ii%.*}.mjs";
-
-	# Fix imports in ESM files (we first remove existing extensions)
-	sed -Ei 's/\s+from\s+"\.\/(.+)\.m?js";/ from ".\/\1";/g' "${ii%.*}.mjs";
-	sed -Ei 's/\s+from\s+"\.\/(.+)";/ from ".\/\1.mjs";/g' "${ii%.*}.mjs";
 done
