c74faa51fae871e7eddb4908a189f87e092e61b8
Andres Correa Casablanca <castarco@coderspirit.xyz>
2022-08-19 19:48:52 +0200

    fix: make build scripts fail fast on error
    Signed-off-by: Andres Correa Casablanca <castarco@coderspirit.xyz>

--- scripts/build_cjs.sh
+++ scripts/build_cjs.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+set -e;
+
 tsc -p ./tsconfig.cjs.json && \
 for i in $( ls ./dist/cjs/*.js ); do mv $i ${i%.*}.cjs; done && \
 for i in $( ls ./dist/cjs/*.js.map ); do
-  ii="${i%.*}"
-  mv $i ${ii%.*}.cjs.map;
+	ii="${i%.*}"
+	mv $i ${ii%.*}.cjs.map;
 
 	# Fix source file refences inside maps
 	sed -Ei 's/"file":"(.+)\.js"/"file":"\1.cjs"/g' "${ii%.*}.cjs.map";
