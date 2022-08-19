3955629d1634b0002c7bc55bea67ced6009d991b
Andres Correa Casablanca <castarco@coderspirit.xyz>
2022-08-19 21:30:49 +0200

    fix: support for bsd sed
    Signed-off-by: Andres Correa Casablanca <castarco@coderspirit.xyz>

--- scripts/build_cjs.sh
+++ scripts/build_cjs.sh
@@ -5,13 +5,24 @@ set -e;
 # Compile
 tsc -p ./tsconfig.cjs.json;
 
+# Sed works differently depending on whether it's the BSD or GNU variant
+if [ "$(sed --version 2>/dev/null | grep GNU | wc -l)" -gt "0" ]; then
+	SED_VARIANT="GNU"
+else
+	SED_VARIANT="BSD"
+fi
+
 # Process source files
 for i in $( ls ./dist/cjs/*.js ); do
 	# Rename source files
 	mv $i ${i%.*}.cjs;
 
 	# Fix map references inside source files
-	sed -Ei 's/\/\/# sourceMappingURL=(.+)\.js\.map/\/\/# sourceMappingURL=\1.cjs.map/g' "${i%.*}.cjs";
+	if [ "${SED_VARIANT}" = "BSD" ]; then
+		sed -E -i '' 's/\/\/#[[:space:]]sourceMappingURL=(.+)\.js\.map/\/\/# sourceMappingURL=\1.cjs.map/g' "${i%.*}.cjs";
+	else
+		sed -Ei 's/\/\/# sourceMappingURL=(.+)\.js\.map/\/\/# sourceMappingURL=\1.cjs.map/g' "${i%.*}.cjs";
+	fi;
 done;
 
 # Process source maps
@@ -21,5 +32,9 @@ for i in $( ls ./dist/cjs/*.js.map ); do
 	mv $i ${ii%.*}.cjs.map;
 
 	# Fix source file refences inside maps
-	sed -Ei 's/"file":"(.+)\.js"/"file":"\1.cjs"/g' "${ii%.*}.cjs.map";
-done
+	if [ "${SED_VARIANT}" = "BSD" ]; then
+		sed -E -i '' 's/"file":"(.+)\.js"/"file":"\1.cjs"/g' "${ii%.*}.cjs.map";
+	else
+		sed -Ei 's/"file":"(.+)\.js"/"file":"\1.cjs"/g' "${ii%.*}.cjs.map";
+	fi;
+done;
--- scripts/build_esm.sh
+++ scripts/build_esm.sh
@@ -2,25 +2,48 @@
 
 set -e;
 
+# Compile
 tsc -p ./tsconfig.esm.json;
 
+# Sed works differently depending on whether it's the BSD or GNU variant
+if [ "$(sed --version 2>/dev/null | grep GNU | wc -l)" -gt "0" ]; then
+	SED_VARIANT="GNU"
+else
+	SED_VARIANT="BSD"
+fi
+
+# Process source files
 for i in $( ls ./dist/esm/*.js ); do
 	# Rename source files
 	mv $i ${i%.*}.mjs;
 
-	# Fix map references inside source files
-	sed -Ei 's/\/\/# sourceMappingURL=(.+)\.js\.map/\/\/# sourceMappingURL=\1.mjs.map/g' "${i%.*}.mjs";
+	if [ "${SED_VARIANT}" = "BSD" ]; then
+		# Fix map references inside source files
+		sed -E -i '' 's/\/\/#[[:space:]]sourceMappingURL=(.+)\.js\.map/\/\/# sourceMappingURL=\1.mjs.map/g' "${i%.*}.mjs";
 
-	# Fix imports in ESM files (we first remove existing extensions)
-	sed -Ei 's/\s+from\s+"\.\/(.+)\.m?js";/ from ".\/\1";/g' "${i%.*}.mjs";
-	sed -Ei 's/\s+from\s+"\.\/(.+)";/ from ".\/\1.mjs";/g' "${i%.*}.mjs";
+		# Fix imports in ESM files (we first remove existing extensions)
+		sed -E -i '' 's/[[:space:]]+from[[:space:]]+'"'"'\.\/(.+)\.m?js'"'"';/ from '"'"'.\/\1'"'"';/g' "${i%.*}.mjs";
+		sed -E -i '' 's/[[:space:]]+from[[:space:]]+'"'"'\.\/(.+)'"'"';/ from '"'"'.\/\1.mjs'"'"';/g' "${i%.*}.mjs";
+	else
+		# Fix map references inside source files
+		sed -Ei 's/\/\/# sourceMappingURL=(.+)\.js\.map/\/\/# sourceMappingURL=\1.mjs.map/g' "${i%.*}.mjs";
+
+		# Fix imports in ESM files (we first remove existing extensions)
+		sed -Ei 's/\s+from\s+'"'"'\.\/(.+)\.m?js'"'"';/ from '"'"'.\/\1'"'"';/g' "${i%.*}.mjs";
+		sed -Ei 's/\s+from\s+'"'"'\.\/(.+)'"'"';/ from '"'"'.\/\1.mjs'"'"';/g' "${i%.*}.mjs";
+	fi;
 done;
 
+# Process source maps
 for i in $( ls ./dist/esm/*.js.map ); do
 	# Rename sourcemap files
 	ii="${i%.*}"
 	mv $i "${ii%.*}.mjs.map";
 
 	# Fix source file refences inside maps
-	sed -Ei 's/"file":"(.+)\.js"/"file":"\1.mjs"/g' "${ii%.*}.mjs.map";
+	if [ "${SED_VARIANT}" = "BSD" ]; then
+		sed -E -i '' 's/"file":"(.+)\.js"/"file":"\1.mjs"/g' "${ii%.*}.mjs.map";
+	else
+		sed -Ei 's/"file":"(.+)\.js"/"file":"\1.mjs"/g' "${ii%.*}.mjs.map";
+	fi;
 done
