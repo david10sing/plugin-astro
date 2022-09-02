9ecae04eab6d81bca40a5e15c34727d8f0cdcdb1
Andres Correa Casablanca <castarco@coderspirit.xyz>
2022-09-02 08:31:34 +0200

    fix: create assets dir when it does not exist
    when running in minimal astro configurations we can't be sure that the
    `assets` dir already exists by the time we want to place files in it, so
    we have to ensure its existence.
    
    Signed-off-by: Andres Correa Casablanca <castarco@coderspirit.xyz>

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import {
 	insert as insertIntoLyraDB,
 	save as saveLyraDB,
 } from '@lyrasearch/lyra'
-import { readFileSync, writeFileSync } from 'node:fs'
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'node:fs'
 import { compile } from 'html-to-text'
 import { join as joinPath } from 'node:path'
 
@@ -103,11 +103,16 @@ export const createPlugin = (
 				config = cfg
 			},
 			'astro:build:done': ({ pages, routes }) => {
+				const assetsDir = joinPath(config.outDir.pathname, 'assets')
+				if (!existsSync(assetsDir)) {
+					mkdirSync(assetsDir)
+				}
+
 				for (const [dbName, dbConfig] of Object.entries(options)) {
 					const namedDb = prepareLyraDb(dbConfig, pages, routes)
 
 					writeFileSync(
-						joinPath(config.outDir.pathname, 'assets', `lyraDB_${dbName}.json`),
+						joinPath(assetsDir, `lyraDB_${dbName}.json`),
 						JSON.stringify(saveLyraDB(namedDb)),
 						{ encoding: 'utf8' },
 					)
